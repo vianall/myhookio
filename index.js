331f0a1c59b570eeb08fa7fa4ce9c8c88205a538
Alvian Nugroho <69709204+vianall@users.noreply.github.com>
2023-03-07 06:40:29 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) ,=> {
+  res.send('It:s working!');
+});
+
+app.post('/webhook', async (req, res) => {
+  try {
+    // Mengirim req.body menggunakan Axios
+    const response = await axios.post('https://api.bots.business/v1/bots/819550/new-webhook?&command=%2FonIncome&public_user_token=2c47674b5c8e7740763f2477f749e14d&user_id=2711154', req.body);
+
+    // Mengembalikan response dari Axios ke client
+    res.send(response.data);
+  } catch (error) {
+    // Mengirim error ke client
+    res.send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
